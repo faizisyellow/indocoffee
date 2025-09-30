b4ff4f717c35ced9dd31235f009053f00faf14ed
faizisyellow <114222238+faizisyellow@users.noreply.github.com>
2025-09-30 13:05:47 +0700

    add: Update product test

--- internal/repository/products/products_test.go
+++ internal/repository/products/products_test.go
@@ -12,6 +12,10 @@ import (
 )
 
 func TestProductsWithRealDB(t *testing.T) {
+	if getEnvironment(t) != "development" {
+		t.Skip("skipping test: only runs in development environment")
+	}
+
 	products.Contract{func() (products.Products, func()) {
 		newDB, err := setupTestDB(t)
 		if err != nil {
@@ -40,6 +44,16 @@ func setupTestDB(t *testing.T) (*sql.DB, error) {
 		"1m",
 		"1m",
 	)
+}
+
+func getEnvironment(t *testing.T) string {
+	t.Helper()
+	err := loadEnv()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return os.Getenv("ENV")
 }
 
 func loadEnv() error {
